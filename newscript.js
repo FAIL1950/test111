859c709a14f4626b0158ed2438b4d8054aae22ea
FAIL1950 <144266349+FAIL1950@users.noreply.github.com>
2024-08-15 18:59:29 +0300

    Add files via upload

--- newscript.js
+++ newscript.js
@@ -1,50 +1,65 @@
-let elm = document.getElementById("photo");
-elm.style.position = "absolute";
+const elements = document.querySelectorAll('.photo');
+console.log(elements);
 let startX, startY, initialX, initialY;
-window.addEventListener('wheel', (event) => {
-    let currentWidth = window.getComputedStyle(elm).width;
-    let numericWidth = parseFloat(currentWidth);
-    let newWidth;
-    if (event.deltaY < 0) {
-        console.log('scrolling up');
-        newWidth = numericWidth + 50;
-    }
-    else if (event.deltaY > 0) {
-        console.log('scrolling down');
-        newWidth = numericWidth - 50;
-    }
-    elm.style.width = newWidth + 'px';
+elements.forEach((item) => {
+    item.style.position = "absolute";
+    const myHammer = new Hammer(item);
+    item.addEventListener('wheel', (event) => {
+        event.preventDefault();
+        let currentWidth = window.getComputedStyle(item).width;
+        let numericWidth = parseFloat(currentWidth);
+        let newWidth;
+
+        if (event.deltaY < 0) {
+            newWidth = numericWidth + 10;
+        }
+        else if (event.deltaY > 0) {
+            newWidth = numericWidth - 10;
+        }
+        item.style.width = newWidth + 'px';
+
+    });
+
+    myHammer.on('panstart', function (event) {
+        startX = event.center.x;
+        startY = event.center.y;
+        initialX = item.offsetLeft;
+        initialY = item.offsetTop;
+    });
+
+    myHammer.on('panmove', function (event) {
+        let dx = event.center.x - startX;
+        let dy = event.center.y - startY;
+
+        item.style.left = initialX + dx + 'px';
+        item.style.top = initialY + dy + 'px';
+    });
+
+    let scale = 1;
+    let startScale = 1;
+    myHammer.get('pinch').set({ enable: true });
+
+    myHammer.on('pinchstart', function (event) {
+        startScale = scale;
+    });
+    
+    
+    myHammer.on('pinch', function (event) {
+        scale = startScale * event.scale;
+        item.style.transform = 'scale(' + scale + ')';
+    });
 });
 
-let myHammer = new Hammer(elm);
-myHammer.get('pinch').set({ enable: true });
-myHammer.get('pan').set({ direction: Hammer.DIRECTION_ALL });
-
-myHammer.on('panstart', function (event) {
-    startX = event.center.x;
-    startY = event.center.y;
-    initialX = elm.offsetLeft;
-    initialY = elm.offsetTop;
-});
-
-myHammer.on('panmove', function (event) {
-    let dx = event.center.x - startX;
-    let dy = event.center.y - startY;
-
-    elm.style.left = initialX + dx + 'px';
-    elm.style.top = initialY + dy + 'px';
-});
-
-let scale = 1;
-let startScale = 1;
 
 
-myHammer.on('pinchstart', function (event) {
-    startScale = scale;
-});
 
 
-myHammer.on('pinch', function (event) {
-    scale = startScale * event.scale;
-    elm.style.transform = 'scale(' + scale + ')';
-});
+
+
+
+
+
+
+
+
+
